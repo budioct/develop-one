ffb65d8c42ba127e4ca4ecda5798bbd408d5bf52
Budhi Octaviansyah <budioct@gmail.com>
2024-12-27 19:57:35 +0700

    start project

---
+++ src/main/java/budhioct/dev/FullStackSpringBootApplication.java
@@ -0,0 +1,13 @@
+package budhioct.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackSpringBootApplication.class, args);
+	}
+
+}
