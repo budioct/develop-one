cc39b32fd8ae7978a0bec58484ccba39303f0b7b
Budhi Octaviansyah <budioct@gmail.com>
2024-12-29 10:56:06 +0700

    add user

---
+++ src/main/java/budhioct/dev/entity/User.java
@@ -0,0 +1,42 @@
+package budhioct.dev.entity;
+
+import budhioct.dev.security.role.Role;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createAt;
+    @LastModifiedDate
+    @Column(name = "updated_at", insertable = false)
+    private LocalDateTime updateAt;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Token> tokens;
+
+}
