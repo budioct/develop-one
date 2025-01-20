124a39c93c7746ee2d47171cf2951ff72d483853
Budhi Octaviansyah <budioct@gmail.com>
2025-01-21 00:01:40 +0700

    add view: register and login

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,18 +1,96 @@
 <script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import axios from "axios";
+import { useField, useForm } from "vee-validate";
+import * as yup from "yup";
 
+// schema validator used Yup
+const schema = yup.object({
+  email: yup.string().email("Email is invalid").required("Email is required"),
+  password: yup.string().required("Password is required"),
+});
+
+// used `useForm` to validator vee-validate
+const { handleSubmit, setErrors, errors, isSubmitting } = useForm({
+  validationSchema: schema,
+  validateOnSubmit: true,
+  validateOnBlur: false,
+  validateOnChange: false,
+});
+
+// used `useField` to validator vee-validate
+const { value: email } = useField("email");
+const { value: password } = useField("password");
+
+const router = useRouter();
+
+// state for error message from backend
+const apiError = ref("");
+
+// sumbit
+const submit = handleSubmit(async (values) => {
+  try {
+    const response = await axios.post("http://localhost:8080/api/v1/auth/login", values, {
+      withCredentials: true,
+    });
+
+    // if success login, set token and redirect
+    axios.defaults.headers.common["Authorization"] = `Bearer ${response.data.token}`;
+    await router.push("/");
+
+  } catch (error) {
+    if (error.response && error.response.data) {
+      const { status_code, errors } = error.response.data;
+
+      // set error message to state
+      if (status_code === 401) {
+        apiError.value = errors;
+      } else {
+        apiError.value = "An unexpected error occurred. Please try again.";
+      }
+    }
+  }
+});
 </script>
 
 <template>
-
-  <form class="form-signin">
+  <form @submit.prevent="submit" class="form-signin">
     <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-    <label for="inputEmail" class="sr-only">Email address</label>
-    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-    <label for="inputPassword" class="sr-only">Password</label>
-    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-  </form>
 
+    <!-- Alert for error from API -->
+    <div v-if="apiError" class="alert alert-danger" role="alert">
+      {{ apiError }}
+    </div>
+
+    <!-- Email Field -->
+    <label for="inputEmail" class="sr-only">Email address</label>
+    <input
+        v-model="email"
+        type="email"
+        id="inputEmail"
+        class="form-control"
+        placeholder="Email address"
+        :class="{ 'is-invalid': errors.email }"
+    >
+    <span v-if="errors.email" class="text-danger">{{ errors.email }}</span>
+
+    <!-- Password Field -->
+    <label for="inputPassword" class="sr-only">Password</label>
+    <input
+        v-model="password"
+        type="password"
+        id="inputPassword"
+        class="form-control"
+        placeholder="Password"
+        :class="{ 'is-invalid': errors.password }"
+    />
+    <span v-if="errors.password" class="text-danger">{{ errors.password }}</span>
+
+    <button class="btn btn-lg btn-primary btn-block mt-3" type="submit" :disabled="isSubmitting">
+      Sign in
+    </button>
+  </form>
 </template>
 
 <style scoped>
