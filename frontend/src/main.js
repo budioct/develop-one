858d85587c37e3cc1713ad2a1d6c906d65713b49
Budhi Octaviansyah <budioct@gmail.com>
2025-01-21 18:43:41 +0700

    add package and store pinia to entry point component

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,15 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { router } from './router'
+import { createPinia } from "pinia";
+import { useAuthStore } from "./stores/authStore.js";
 
-createApp(App)
-    .use(router)
-    .mount('#app')
+const app = createApp(App);
+const pinia = createPinia();
+app.use(pinia);
+app.use(router);
+
+const authStore = useAuthStore();
+authStore.InterceptorToken();
+
+app.mount("#app");
