1a025885f05eb50f9205e57beac3a305d45119a6
Budhi Octaviansyah <budioct@gmail.com>
2025-01-21 18:47:14 +0700

    update view home, collect data from pinia global state

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,10 +1,24 @@
 <script setup>
+import { useAuthStore } from "../stores/authStore";
+
+const authStore = useAuthStore();
+
+// State for showed login info
+const isLoggedIn = authStore.isAuthenticated;
+const token = authStore.token;
 
 </script>
 
 <template>
   <div class="container">
-    Home
+    <h1>Login Status</h1>
+    <p v-if="isLoggedIn">You are logged in! 🎉</p>
+    <p v-else>You are not logged in.</p>
+
+    <div v-if="token">
+      <h3>Your Token:</h3>
+      <p>{{ token }}</p>
+    </div>
   </div>
 </template>
 
