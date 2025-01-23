601758732465e32fd653f1fd92f492a1cd8a6805
Budhi Octaviansyah <budioct@gmail.com>
2025-01-23 13:02:52 +0700

    add refresh_token state pinia

--- frontend/src/stores/authStore.js
+++ frontend/src/stores/authStore.js
@@ -4,29 +4,34 @@ import axios from 'axios';
 export const useAuthStore = defineStore('auth', {
     state: () => ({
         token: localStorage.getItem("authToken") || null, // collect token from LocalStorage
+        refreshToken: localStorage.getItem("authRefreshToken") || null, // Tambahkan refresh token
         isAuthenticated: !!localStorage.getItem("authToken"), // check does token there is
     }),
     actions: {
-        setToken(token) {
+        setToken(token, refreshToken) {
             this.token = token;
+            this.refreshToken = refreshToken;
             this.isAuthenticated = !!token; // true if token there is
 
             // save token to LocalStorage
             if (token) {
                 axios.defaults.headers.common['Authorization'] = `Bearer ${token}`; // Set header Authorization
                 localStorage.setItem("authToken", token);
+                localStorage.setItem("authRefreshToken", refreshToken);
             } else {
-                localStorage.removeItem("authToken");
+                //localStorage.removeItem("authToken");
+                this.clearToken();
             }
         },
         clearToken() {
             this.token = null;
+            this.refreshToken = null;
             this.isAuthenticated = false;
-            delete axios.defaults.headers.common['Authorization'];
 
             // delete header Authorization  and token from LocalStorage
             delete axios.defaults.headers.common["Authorization"];
             localStorage.removeItem("authToken");
+            localStorage.removeItem("authRefreshToken");
         },
         InterceptorToken() {
             // add interceptor for include token to each request
@@ -41,6 +46,39 @@ export const useAuthStore = defineStore('auth', {
                     return Promise.reject(error);
                 }
             );
+        },
+        InterceptorRefreshToken(){
+            axios.interceptors.response.use(
+                (response) => response, // Biarkan response berhasil lewat
+                async (error) => {
+                    const originalRequest = error.config;
+                    if (error.response && error.response.status === 401 && !originalRequest._retry) {
+                        originalRequest._retry = true;
+                        try {
+                            // Coba refresh token
+                            const response = await axios.post(
+                                "http://localhost:8080/api/v1/auth/token/refresh",
+                                {},
+                                {
+                                    withCredentials: true,
+                                    headers: {
+                                        "Content-Type": "application/json",
+                                        "Authorization": `Bearer ${this.token}`,
+                                    }
+                                }
+                            );
+                            const { access_token, refresh_token } = response.data.data; // Update token
+                            this.setToken(access_token, refresh_token);
+                            originalRequest.headers["Authorization"] = `Bearer ${token}`;
+                            return axios(originalRequest); // Coba ulang request asli
+                        } catch (refreshError) {
+                            this.clearToken(); // Jika gagal, logout
+                            return Promise.reject(refreshError);
+                        }
+                    }
+                    return Promise.reject(error);
+                }
+            )
         }
     },
 });
