ef73db7e012bfe83e1e4fda9bdfe5a3622f963bb
Budhi Octaviansyah <budioct@gmail.com>
2025-01-23 12:13:50 +0700

    update view: add button logout

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,35 +1,63 @@
 <script setup>
+import {computed} from "vue";
+import {useRouter} from "vue-router";
+import {useAuthStore} from "../stores/authStore.js";
+import axios from "axios";
+
+const router = useRouter();
+const authStore = useAuthStore();
+const isAuthenticated = computed(() => authStore.isAuthenticated);
+
+async function logout() {
+  try {
+    const response = await axios.post(
+        "http://localhost:8080/api/v1/auth/logout",
+        {}, // request body
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${authStore.token}`, // insert token from store (pinia global state)
+          },
+        }
+    );
+    // console.log(response); // debugging response logout
+  } catch (error) {
+    console.log("Headers:", error.response?.headers);
+    console.error("Logout failed:", error.response?.data?.message || error.message);
+  } finally {
+    authStore.clearToken(); // delete token from store (pinia global state) and localstorage
+    await router.push({name: "auth-login"}); // redirect to login
+  }
+}
 
 </script>
 
 <template>
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
-    <router-link :to="{name: 'home'}" class="navbar-brand">Home</router-link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
+    <div v-if="isAuthenticated">
+      <router-link :to="{name: 'home'}" class="navbar-brand">Home</router-link>
+    </div>
     <div class="collapse navbar-collapse" id="navbarsExample09">
       <ul class="navbar-nav mr-auto">
-<!--        <li class="nav-item active">-->
-<!--          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>-->
-<!--        </li>-->
-        <li class="nav-item">
+        <li class="nav-item" v-if="!isAuthenticated">
           <router-link :to="{name: 'auth-login'}" class="nav-link">Login</router-link>
         </li>
-        <li class="nav-item">
+        <li class="nav-item" v-if="!isAuthenticated">
           <router-link :to="{name: 'auth-register'}" class="nav-link">Register</router-link>
         </li>
       </ul>
+      <div v-if="isAuthenticated">
+        <div class="form-inline my-2 my-md-0">
+          <button @click="logout" type="button" class="btn btn-outline-danger">Logout</button>
+        </div>
+      </div>
     </div>
   </nav>
 
 </template>
 
 <style scoped>
-.router-link-exact-active {
-  color: #817272 !important;
-  font-weight: bold;
-}
+
 </style>
