dc8003b89dd12e8f4ed79111ebcf3b85552a1d87
Budhi Octaviansyah <budioct@gmail.com>
2025-01-24 10:42:46 +0700

    update: add package bootstrap, and change style css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import { router } from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { router } from './router';
 import { createPinia } from "pinia";
 import { useAuthStore } from "./stores/authStore.js";
+import ToastNotification from 'vue-toast-notification';
+
+import './assets/css/main.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'vue-toast-notification/dist/theme-default.css';
 
 const app = createApp(App);
 const pinia = createPinia();
 app.use(pinia);
 app.use(router);
+app.use(ToastNotification);
 
 const authStore = useAuthStore();
-// authStore.InterceptorToken();
 authStore.InterceptorRefreshToken();
 
 app.mount("#app");
