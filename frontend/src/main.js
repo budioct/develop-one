a07f72f4b76f9bd66875b346b5490f4357244778
Budhi Octaviansyah <budioct@gmail.com>
2025-01-25 21:44:08 +0700

    update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,18 +3,18 @@ import App from './App.vue';
 import { router } from './router';
 import { createPinia } from "pinia";
 import { useAuthStore } from "./stores/authStore.js";
-import ToastNotification from 'vue-toast-notification';
+import ToastPlugin from 'vue-toast-notification';
 
 import './assets/css/main.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import 'vue-toast-notification/dist/theme-default.css';
+import 'vue-toast-notification/dist/theme-bootstrap.css';
 
 const app = createApp(App);
 const pinia = createPinia();
 app.use(pinia);
 app.use(router);
-app.use(ToastNotification);
+app.use(ToastPlugin);
 
 const authStore = useAuthStore();
 authStore.InterceptorRefreshToken();
